8a106f32d46fee3b3f186034cf25646e0fe16b0a
Sander Øverås <sander.overas@gmail.com>
2026-09-05 13:52:21 +0200

    setup part 2
    more or less everything for second assingment is done

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject canvas;
 
-    // Update is called once per frame
-    void Update()
+    public void CloseCanvas()
     {
-        
+        canvas.SetActive(false);
     }
 }
